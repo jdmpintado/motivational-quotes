f4e40bb28546a3bf3c6a8583e92f64f2a2ca3c93
jdmpintado <137705155+jdmpintado@users.noreply.github.com>
2025-07-03 17:34:05 +0100

    Added the AI API integration

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-//function displayQuote()
-
-function generateQuote(event) {
-  event.preventDefault();
-
+function displayQuote(response) {
   new Typewriter("#quote", {
-    strings: "Here you have a motivational quote about the topic you submitted",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateQuote(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let prompt = `User instructions: generate a motivational quote regarding ${instructionsInput.value}`;
+  let context =
+    "You are an excepcional motivational leader, who inspire people through the wise words you use when they reach out to you, while they are facing challenges. Your mission is to generate a motivational quote according to the topic instructed by the user. You can use no more than 4 lines to display the quote. Display the quote in basic HTML (without mentioning it), in a <p> element. After the quote, in a separate paragraph, sign as 'SheCodes AI'. Place the signature inside a <strong> element.";
+  let apiKey = "c450930fa53b90f8c5ab74d6t08ao678";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayQuote);
 }
 
 let quoteFormElement = document.querySelector("#quote-generator-form");
